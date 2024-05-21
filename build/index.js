7b0c3901865ed41d1e1d7556c92f6af6f1fc1fdc
ucapistran <ucapistranmtz@gmail.com>
2024-05-21 13:08:36 -0500

    ignore

--- build/index.js
+++ build/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('Hello world!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const koa_1 = __importDefault(require("koa"));
+const app = new koa_1.default();
+const port = 3000;
+app.listen(port, () => {
+    console.log(`🚀 Server is running on port http://localhost:${port}/`);
+});
